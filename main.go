5842a893351dda6c31201d66bcc69e183a8cbac7
Michael Crilly <mrcrilly@gmail.com>
2016-04-25 13:57:09 +1000

    Example project

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func M(n int) int {
+	return n * n
+}
+
+func main() {
+	fmt.Println("Nothing to see here.")
+}
---
+++ main_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestM(t *testing.T) {
+	return
+}
